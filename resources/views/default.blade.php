9d3f387db10962abf7362832a1e6326415fccc79
Tarun Teja Gotimukala <tarunteja@gmail.com>
2015-05-24 09:49:37 +0000

    change input size to 220px

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -58,10 +58,10 @@
           								 <form id="addartist-nav" class="form" accept-charset="UTF-8">
 														<input type="hidden" name="_token" value="{{ csrf_token() }}">
           										<div class="form-group">
-          											 <input type="text" id="artist-name" name="artist_name" class="form-control" maxlength="30" placeholder="Artist Name" required>
+          											 <input type="text" id="artist-name" name="artist_name" class="form-control form-sz" maxlength="30" placeholder="Artist Name" required>
           										</div>
           										<div class="form-group">
-          											 <button type="submit" class="btn btn-primary btn-block">Add Artist</button>
+          											 <button type="submit" class="btn btn-primary btn-block form-sz">Add Artist</button>
           										</div>
           								 </form>
           							</div>
