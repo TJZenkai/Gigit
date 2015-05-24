f5f7b59a3ab546d230c59a17d46a82093d6f0a11
Tarun Teja Gotimukala <tarunteja@gmail.com>
2015-05-24 09:35:16 +0000

    change stylesheet add method

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -6,12 +6,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Gigit</title>
 
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-  <link href="{{ asset('/css/main.css') }}" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link href="css/app.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
 	<meta name="csrf-token" content="{{ csrf_token() }}" />
 	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
